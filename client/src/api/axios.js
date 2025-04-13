effbed7cb7aae121f4f15352c4a4ac7e67b28e75
Abhishek Kumar <sahniak9868@gmail.com>
2025-04-13 23:06:38 +0530

    Fix API base URL configuration for production and development environments

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 const isProduction = import.meta.env.VITE_NODE_ENV === 'production';
-console.log(import.meta.env.VITE_NODE_ENV); // Check if in production mode
 
 const instance = axios.create({
-    baseURL: isProduction ? 'https://full-stack-e-wallet.vercel.app/api' : 'http://localhost:5000/api',
+    baseURL: isProduction ? 'http://localhost:5000/api' : 'https://full-stack-e-wallet.vercel.app/api',
 
     withCredentials: true, // ⬅️ Important for sending cookies
 });
